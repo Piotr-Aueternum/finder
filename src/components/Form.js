97e86e2393d27da433c08bf11c5bdb92eb910b45
piotr-aueternum <piotr.aueternum@gmail.com>
2017-06-16 12:53:09 +0200

    Finished up filter form connection with redux, remove depedency hell posibilities with dotenv pkg

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,42 +2,66 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Select from './Select';
 
-const categories = [];
-
-const brands = [];
-
 export default class extends React.Component {
   static propTypes = {
     onSubmit: PropTypes.func.isRequired,
+    inputs: PropTypes.arrayOf(PropTypes.object).isRequired,
+    formSubmit: PropTypes.func.isRequired,
   }
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { timer: 0, data: {} };
     this.onSubmit = this.onSubmit.bind(this);
+    this.onSelectChange = this.onSelectChange.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
   }
   onSubmit(e) {
     e.preventDefault();
     this.props.onSubmit();
   }
+  onInputChange(e) {
+    const { name, value } = e.target;
+    const { data } = this.state;
+    this.setState({ data: { ...data, [name]: value }, timer: 5 });
+    clearTimeout(this.countdown);
+    this.tick();
+  }
+  onSelectChange(e) {
+    const { name, value } = e.target;
+    const { formSubmit } = this.props;
+    this.setState({ data: { ...this.state.data, [name]: value }, timer: 0 });
+    clearTimeout(this.countdown);
+    setTimeout(() => formSubmit(this.state.data));
+  }
+  tick() {
+    const { timer } = this.state;
+    const { formSubmit } = this.props;
+    this.countdown = setTimeout(() => {
+      this.setState({ timer: timer - 1 });
+      if (timer > 0) {
+        this.tick();
+      } else {
+        clearTimeout(this.countdown);
+        setTimeout(() => formSubmit(this.state.data));
+      }
+    }, 1000);
+  }
   render() {
-    const { onSubmit } = this.props;
+    const { onSelectChange, onInputChange } = this;
+    const { onSubmit, inputs } = this.props;
     return (
-      <div>
+      <form onSubmit={e => onSubmit(e)}>
         <h1>Filter Panel</h1>
-        <form onSubmit={e => onSubmit(e)}>
-          <input type="text" placeholder="Case sensitive name filter" />
+        <input type="text" name="query" placeholder="Case sensitive name filter" onChange={e => onInputChange(e)} />
+        {inputs.map((item, key) => (
           <Select
-            name="categories"
-            options={categories}
-            onChange={e => (e.target.value)}
+            key={key}
+            name={item.name}
+            options={item.options}
+            onChange={e => onSelectChange(e)}
           />
-          <Select
-            name="brands"
-            options={brands}
-            onChange={e => (e.target.value)}
-          />
-        </form>
-      </div>
+        ))}
+      </form>
     );
   }
 }
--- src/containers/FilterForm.js
+++ src/containers/FilterForm.js
@@ -4,14 +4,12 @@ import Form from '../components/Form';
 
 const mapStateToProps = state => ({
   inputs: state.inputs,
-  options: state.options,
 });
 
 
 const mapDispatchToProps = dispatch => ({
-  onChange: (e) => {
-    const { value, name } = e.target;
-    dispatch(updateFilters({ value, name }));
+  formSubmit: (data) => {
+    dispatch(updateFilters({ ...data }));
   },
 });
 
